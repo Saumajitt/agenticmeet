2ceeb60582316c28dc37ca708c5ea68b69f25fa6
Saumajit Malakar <saumajit15sam@gmail.com>
2026-02-08 20:52:26 +0530

    fix: remove redirect to allow dashboard home page

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async redirects() {
+//       return [
+//         {
+//           source: "/",
+//           destination: "/meetings",
+//           permanent: false,
+//         }
+//       ]
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/meetings",
-          permanent: false,
-        }
-      ]
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
