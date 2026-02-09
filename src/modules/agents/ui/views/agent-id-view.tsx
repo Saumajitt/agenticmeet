5aaa215f283906764e85f5859a81efbb05745ee9
Saumajit Malakar <saumajit15sam@gmail.com>
2026-02-09 09:25:39 +0530

    fix: agent detail page dark mode

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -38,7 +38,7 @@ export const AgentIdView = ({ agentId }: Props) => {
                 await queryClient.invalidateQueries(
                     trpc.premium.getFreeUsage.queryOptions(),
                 );
-               router.push("/agents");
+                router.push("/agents");
             },
             onError: (error) => {
                 toast.error(error.message);
@@ -63,10 +63,10 @@ export const AgentIdView = ({ agentId }: Props) => {
     return (
         <>
             <RemoveConfirmation />
-            <UpdateAgentDialog 
+            <UpdateAgentDialog
                 open={updateAgentDialogOpen}
                 onOpenChange={setUpdateAgentDialogOpen}
-                initialValues={data}  
+                initialValues={data}
             />
             <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
                 <AgentIdViewHeader
@@ -75,7 +75,7 @@ export const AgentIdView = ({ agentId }: Props) => {
                     onEdit={() => setUpdateAgentDialogOpen(true)}
                     onRemove={handleRemoveAgent}
                 />
-                <div className="bg-white rounde-lg border">
+                <div className="bg-card rounded-lg border">
                     <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
                         <div className="flex items-center gap-x-3">
                             <GeneratedAvatar
@@ -89,12 +89,12 @@ export const AgentIdView = ({ agentId }: Props) => {
                             variant="outline"
                             className="flex items-center gap-x-3 [&>svg]:size-4"
                         >
-                            <VideoIcon className="text-blue-700"/>
+                            <VideoIcon className="text-blue-700" />
                             {data.meetingCount} {data.meetingCount === 1 ? "meeting" : "meetings"}
                         </Badge>
                         <div className="flex flex-col gap-y-4">
                             <p className="text-lg font-medium">Instructions</p>
-                            <p className="text-neutral-800">{data.instructions}</p>
+                            <p className="text-muted-foreground">{data.instructions}</p>
                         </div>
                     </div>
                 </div>
@@ -119,5 +119,5 @@ export const AgentIdViewError = () => {
             title="Error Loading Agent"
             description="Something went wrong"
         />
-    );    
+    );
 };    
